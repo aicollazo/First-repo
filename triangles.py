c819fc6b92a689080b5692094161d196fcd01d91
Ari <acollazo@ucvts.org>
2018-12-13 18:15:11 +0000

    Hypotenuse function

--- triangles.py
+++ triangles.py
@@ -1,5 +1,6 @@
 from math import sqrt
-a = float(input("a: "))
-b = float(input("b: "))
-c = sqrt(a**2 + b**2)
-print(f"The hypotenuse is {c}")
+def hypotenuse(a,b):
+    a = float(input("a: "))
+    b = float(input("b: "))
+    c = sqrt(a**2 + b**2)
+    print(f"The hypotenuse is {c}")
